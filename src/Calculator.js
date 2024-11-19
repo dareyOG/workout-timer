dc95ab9c4daf3a8bd9be04842b8050fe31207222
Oludire Damilare <oludire@gmail.com>
2024-11-19 20:05:17 +0100

    ✅helper functions in effects

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,6 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, memo } from 'react';
 import clickSound from './ClickSound.m4a';
-import { memo } from 'react';
 
 function Calculator({ workouts, allowSound }) {
   const [number, setNumber] = useState(workouts.at(0).numExercises);
@@ -17,15 +16,22 @@ function Calculator({ workouts, allowSound }) {
     [number, sets, speed, durationBreak]
   );
 
+  useEffect(
+    function () {
+      const playSound = function () {
+        if (!allowSound) return;
+        const sound = new Audio(clickSound);
+        sound.play();
+      };
+
+      playSound();
+    },
+    [duration, allowSound]
+  );
+
   // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
-
-  const playSound = function () {
-    if (!allowSound) return;
-    const sound = new Audio(clickSound);
-    sound.play();
-  };
 
   const handleInc = () => {
     setDuration(duration => Math.floor(duration) + 1);
